deefb986e2634f4a65f5c906953d075c06fbe076
= <=>
2024-01-25 23:49:24 +0000

    Added Version Comment a the top of page Acl.bash

--- Includes/Acl.bash
+++ Includes/Acl.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#V1 Script
+#Added Function SetAcl DeleteAcl and GetAcl function to script
+#These scripts are just shorthand codes to make my own code a little easier
+#25/01/2024
+
+
 # The following Commands will set Advanced user permissions allowing
 # More users and groups access to folders.
 
