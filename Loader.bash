db164079ba9c0e2a1e2a5ed045e7f585e36240fa
= <=>
2024-02-13 19:09:11 +0000

    Renamed Load.bash to Loader.bash

---
+++ Loader.bash
@@ -0,0 +1,26 @@
+#! /bin/usr/env bash
+
+RootDir=/Engine
+# Create folder /Engine/Scripts
+sudo mkdir -p "$RootDir"
+# Set owner and groups and apply permissions this will temporarily be set to root:root
+sudo chown -R root:root "$RootDir"
+sudo chmod -R ug=rwx,o-rwx "$RootDir"
+
+if [ ! -f "Scripts.tar" ]; then
+  echo "No File Exists"
+## wget the Scripts/zip file
+Zipfile=Files.tar.gz
+sudo wget --no-check-certificate "https://github.com/EngineFw/DockerShell/blob/main/$Zipfile"
+## Unzip the file and place it in  the folder /Engine/Sriptsc
+sudo tar -xvf $Zipfile -C "$RootDir"
+fi
+# Wget install.bash
+#if [ !-f "install.bash" ]; then
+#sudo wget --no-check-certificate https://github.com/EngineFw/DockerShell/blob/main/install.bash
+#fi
+## change into the install.bash
+#sudo bash ~/Engine/install.bash
+
+#Delete the download file
+#sudo rm -r ~/Engine/loader.bash
