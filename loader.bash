39620115463d9ca275ef822a3eb3275bd0b84665
= <=>
2024-02-13 23:22:01 +0000

    removed Loader.bash

--- loader.bash
+++ loader.bash
@@ -9,21 +9,18 @@ sudo chmod -R ug=rwx,o-rwx "$RootDir"
 
 if [ ! -f "Scripts.tar" ]; then
   echo "No File Exists"
-## wget the Zips/Files.zip file
+## wget the Scripts/zip file
 Zipfile=Files.tar.gz
 sudo wget --no-check-certificate "https://github.com/EngineFw/DockerShell/blob/main/$Zipfile"
 ## Unzip the file and place it in  the folder /Engine/Sriptsc
 sudo tar -xvf $Zipfile -C "$RootDir"
 fi
-# using Wget download install.bash Raw File
-if [ ! -f "install.bash" ]; then
-wget  https://raw.githubusercontent.com/EngineFw/DockerShell/main/install.bash
+# Wget install.bash
+if [ !-f "install.bash" ]; then
+sudo wget https://github.com/EngineFw/DockerShell/blob/main/install.bash
+# Bash into install.bash
+sudo bash ./install.bash
 fi
-## change into the install.bash
-#sudo bash ~/Engine/install.bash
-
-cat install.bash
-exit() 0
 
 #Delete the download file
-#sudo rm -r ~/Engine/loader.bash
+sudo rm -r ./loader.bash
