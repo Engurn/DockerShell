9a75a86649445ca359a3bb4db25f0b058438eb5d
= <=>
2024-02-20 00:30:04 +0000

    Added Support for New folder exists function

--- Files/Folders.bash
+++ Files/Folders.bash
@@ -1,31 +1,40 @@
 #!/usr/bin/env bash
-RootDir="/Engine"/
+RootDir="/Engine"
 
-list=$folders
-  if [ ! -z "$1" ]; then
-    list="$1"
-  else
-    list=(
-    "$RootDir/Docker/Containers/Sites"
-    "$RootDir/Docker/Containers/Services"
-    "$RootDir/Samba/Confs"
+# Global Variable for Folders
+    folders=(
+    "${RootDir}/Core/Docker/Containers/Sites"
+    "${RootDir}/Core/Docker/Containers/Services"
     )
-  fi
-  
-FolderExist()
+
+FolderPermissions()
 {
-  if [ -d "$1" ]; then
-echo "Folder $2 Exists"
-else
-  sudo mkdir -p $1
-  echo "Folder $1 is created"
-fi
+  folder=$1
+  sudo chown -R root:docker $folder
+  sudo chmod -R ug=rwx,o-rwx $folder
 }
 
+NewFolder()
+{
+    if [ -d "$1" ]; then
+          echo "Folder ${1} Exists "
+          else
+        sudo mkdir -p "$1"
+        echo "folder : $1 Created"
+        fi
+}
+
+  # Iterate over the folders of the array using a for loop
+  # Iterate over the folders of the array using a for loop
+#  Use @ within the for loop as a wildcard to choose all  the rows
 
 
-if [ "$1" == "--new-folder" ]; then
-TestList $list2
+if [ $1 == "--new" ]; then
+echo "coming soon"
 else
-echo "invalid Command"
-fi
+     echo "Found ${#folders[@]} Folders"
+        for folder in ${folders[@]};
+        do
+        NewFolder $folder
+        FolderPermissions $folder;
+        done
